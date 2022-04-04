102389c1d9a5011fd0bd53fd8c9f51b6b7d66b93
Jardelson Feijao <jardelsonfeijao04@gmail.com>
2022-04-03 23:12:23 -0300

    Project created

---
+++ src/main/java/com/jf/prjWebServicesSpringBoot/PrjWebServicesSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.jf.prjWebServicesSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjWebServicesSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjWebServicesSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jf/prjWebServicesSpringBoot/PrjWebServicesSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jf.prjWebServicesSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjWebServicesSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

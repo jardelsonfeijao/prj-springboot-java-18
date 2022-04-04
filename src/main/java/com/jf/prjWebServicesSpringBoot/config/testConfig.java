a31a73c0d59df82c32cf75d31532924123b0b99f
Jardelson Feijao <jardelsonfeijao04@gmail.com>
2022-04-04 03:20:29 -0300

    OrderItem, many-to-many association with extra attributes - Parte 2

--- src/main/java/com/jf/prjWebServicesSpringBoot/config/testConfig.java
+++ src/main/java/com/jf/prjWebServicesSpringBoot/config/testConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.jf.prjWebServicesSpringBoot.entities.Category;
 import com.jf.prjWebServicesSpringBoot.entities.Order;
+import com.jf.prjWebServicesSpringBoot.entities.OrderItem;
 import com.jf.prjWebServicesSpringBoot.entities.Product;
 import com.jf.prjWebServicesSpringBoot.entities.User;
 import com.jf.prjWebServicesSpringBoot.entities.enums.OrderStatus;
 import com.jf.prjWebServicesSpringBoot.repositories.CategoryRepository;
+import com.jf.prjWebServicesSpringBoot.repositories.OrderItemRepository;
 import com.jf.prjWebServicesSpringBoot.repositories.OrderRepository;
 import com.jf.prjWebServicesSpringBoot.repositories.ProductRepository;
 import com.jf.prjWebServicesSpringBoot.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class testConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -70,6 +75,13 @@ public class testConfig implements CommandLineRunner{
 		
 		userReporitory.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 	
 	

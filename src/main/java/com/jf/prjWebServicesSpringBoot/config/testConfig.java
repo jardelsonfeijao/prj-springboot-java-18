83e28ea6a021a77c67b6aeddd7c71fd864d45a8c
Jardelson Feijao <jardelsonfeijao04@gmail.com>
2022-04-04 19:43:30 -0300

    Payment, one-to-one association

--- src/main/java/com/jf/prjWebServicesSpringBoot/config/testConfig.java
+++ src/main/java/com/jf/prjWebServicesSpringBoot/config/testConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.jf.prjWebServicesSpringBoot.entities.Category;
 import com.jf.prjWebServicesSpringBoot.entities.Order;
 import com.jf.prjWebServicesSpringBoot.entities.OrderItem;
+import com.jf.prjWebServicesSpringBoot.entities.Payment;
 import com.jf.prjWebServicesSpringBoot.entities.Product;
 import com.jf.prjWebServicesSpringBoot.entities.User;
 import com.jf.prjWebServicesSpringBoot.entities.enums.OrderStatus;
@@ -82,6 +83,11 @@ public class testConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
